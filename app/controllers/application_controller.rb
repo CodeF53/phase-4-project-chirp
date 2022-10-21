fe113a9ff04891f49f56fcb6249f0fd543674cc5
F53 <fseusb@gmail.com>
2022-10-21 11:26:51 -0600

    fix auth and add temp user shit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,9 @@ class ApplicationController < ActionController::API
   private
 
   def authorize
-    @current_user = User.find_by(id: session[:user_id])
+    # @current_user = User.find_by(id: session[:user_id])
+    # ! TEMP "ADMIN" ACCOUNT THAT SKIPS AUTH
+    @current_user = User.find_by(id: 1)
 
     render json: { errors: ['Not authorized'] }, status: :unauthorized unless @current_user
   end
