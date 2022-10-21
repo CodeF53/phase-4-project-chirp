b2fc3cf9aecaa0e6ff5e45c164f0c6d4a752c504
F53 <fseusb@gmail.com>
2022-10-21 10:11:03 -0600

    add frontend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,14 @@ class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordInvalid, with: :invalid_record_response
   rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
 
+  before_action :authorize
+
   private
 
   def authorize
     @current_user = User.find_by(id: session[:user_id])
 
-    render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
+    render json: { errors: ['Not authorized'] }, status: :unauthorized unless @current_user
   end
 
   def invalid_record_response(invalid)
---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { LoginSignup } from './pages/LoginSignup';
+
+function App() {
+  const [user, setUser] = useState(null)
+  // auto-login
+  useEffect(() => {fetch("/me").then((r) => { if (r.ok) {
+    r.json().then((user) => setUser(user));
+  }});}, []);
+
+  if (!user) {
+    return <Routes>
+      {/* route user to login if url is login */}
+      <Route path="/login" element={<LoginSignup isLogin={true}  user={user} setUser={setUser} />} />
+      {/* but default to always directing to signup */}
+      <Route path="*"      element={<LoginSignup isLogin={false} user={user} setUser={setUser} />} />
+    </Routes>
+  }
+
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
