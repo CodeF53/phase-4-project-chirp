0a8ed3ee8c7a50114ac7ad656c306e91e079b2fb
F53 <fseusb@gmail.com>
2022-10-23 15:48:11 -0600

    fix auto login

--- client/src/App.js
+++ client/src/App.js
@@ -10,10 +10,11 @@ function App() {
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")))
   useEffect(() => { localStorage.setItem("user", JSON.stringify(user));
   }, [user]);
-  // auto-login (incase cooke expired or something)
-  useEffect(() => {fetch("/me").then((r) => { if (r.ok) {
-    r.json().then((user) => setUser(user));
-  }});}, []);
+  // auto-login (incase cookie expired or something)
+  useEffect(() => {fetch("/me").then((r) => {
+    if (r.ok) { r.json().then((user) => setUser(user)); }
+    else { setUser(null) }
+  });}, []);
 
 
   if (!user) {
