f25776dec2d5a73d7bc4f3c058708462b049b3c9
AustinBarrington <arbarrington12@gmail.com>
2022-10-24 17:39:53 -0600

    profile is looking better

--- client/src/components/LabeledInput.js
+++ client/src/components/LabeledInput.js
@@ -1,7 +1,7 @@
 export function LabeledInput({label, name, type, value, onChange, step, min, max}) {
-    return <div className="row labeled_input">
+    return <div className="labeled_input">
       <label htmlFor={name}>{label}</label>
       <div className="spacer"/>
-      <input name={name} type={type} value={value} onChange={onChange} step={step} min={min} max={max}/>
+      <textarea name={name} type={type} value={value} onChange={onChange} step={step} min={min} max={max}/>
     </div>
   }
--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,25 +1,28 @@
 import { useEffect, useState } from "react";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import '../style/profile.css';
 import { ProfileEditor } from "./ProfileEditor";
+
 
 
 export function Profile ({userData, current_user, toggleEdit}) {
   const [showEditorModal, setShowEditorModal] = useState(false)
   const [user, setUser] = useState({ username:"", icon:"", banner:"", display_name:"", bio:"", website:"", birthday: 0, pinned_chirp_id: 0  })
-  const [following, setFollowing] = useState(false)
+
+  console.log('profile.js userData', userData)
 
   useEffect(() => { fetch(`users/${userData.username}`).then(r=>r.json())
-    .then(data=>{ setUser(data) })}, [])
+    .then(data=>{ setUser(data) })}, [userData])
 
   const isSelf = userData.username === current_user.username
+  const isFollowing = current_user.id === userData.follower_ids
 
   return <div className={`profile userID_${userData.id}`}>
     <div className="banner" style={{backgroundImage:`url(${userData.banner})`}}>
       <img src={userData.icon} alt={`${userData.display_name}'s icon`} className="icon"/>
       <div className="buttons row">
         <div className="spacer"/>
-        {isSelf ? null : <button className="followbtn_container">{following ? "Unfollow" : "Follow"}</button>}
+        {isSelf ? null : <button className="followbtn_container">{isFollowing ? "Unfollow" : "Follow"}</button>}
         {isSelf ? <button className="followbtn_container" onClick={()=>setShowEditorModal(true)}>Edit</button> : null}
       </div>
     </div>
@@ -27,9 +30,12 @@ export function Profile ({userData, current_user, toggleEdit}) {
       <h1 className="display_name">{userData.display_name}</h1>
       <h2 className="username">@{userData.username}</h2>
       <p className="bio">{userData.bio}</p>
+      <a className="website" href={userData.website}>userData.website</a>
+      <p className="birthday">Born on {userData.birthday}</p>
+      <p className="follows">{userData.followers.length} Followers</p>
     </div>
 
-    {showEditorModal?<ProfileEditor userData={userData} toggleEdit={toggleEdit} exit={()=>setShowEditorModal(false)}/>:null}
+    {showEditorModal?<ProfileEditor userData={userData} toggleEdit={()=>toggleEdit()} exit={()=>setShowEditorModal(false)}/>:null}
   </div>
 }
 
