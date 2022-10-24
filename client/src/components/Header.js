93afbcbd72102cb8269d01895336c054851f0119
F53 <fseusb@gmail.com>
2022-10-24 10:23:21 -0600

    improve header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,11 +5,10 @@ export function Header({user}) {
   // TODO: SVGs
   // TODO: logout button
   return <header className="header col">
-    <Link to=""><button>home</button></Link>
-    {/* <button>search</button> */}
-    {/* <button>notifications</button> */}
-    {/* <button>messages</button> */}
-    {/* <button>saved</button> */}
+    <Link to="/"><button>home</button></Link>
+    <button>search</button>
+    <button>notifications</button>
+    <button>messages</button>
     <Link to={user.username}><button>profile</button></Link>
     <button>new chirp</button>
   </header>
