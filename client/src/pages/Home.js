38cdd8291c3c572a890b80dc26adac197ca2f856
F53 <fseusb@gmail.com>
2022-10-26 16:39:59 -0600

    "live" updates

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,9 +5,18 @@ import { ChirpEditor } from "../components/ChirpEditor"
 
 export function Home({current_user}) {
   const [chirp_ids, setChirp_ids] = useState([])
-  useEffect(() => { fetch("feed").then(r=>r.json()).then(data=>{
-    setChirp_ids(data)
-  })}, [])
+
+  const fetchFeed = ()=> fetch("/feed")
+    .then(r=>r.json()).then(data=>{ setChirp_ids(data) })
+
+  useEffect(() => { fetchFeed() }, [])
+
+  // ! temporary "live" (every 5 second) updating
+  useEffect(() => {
+    const interval = setInterval(fetchFeed, 5000);
+    return () => clearInterval(interval);
+    // eslint-disable-next-line
+  }, []);
 
   const removeChirp = chirp_id => setChirp_ids(chirp_ids.filter(id=>id!==chirp_id))
   const addChirp = chirp_id => setChirp_ids([chirp_id, ...chirp_ids])
--- client/src/pages/User.js
+++ client/src/pages/User.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import { Chirps } from "../components/Chirp"
 import { Profile} from "../components/Profile"
-import {Heading} from "../components/Heading"
+import { Heading } from "../components/Heading"
 
 export function User({current_user}) {
   const [userData, setUserData] = useState({chirp_ids:[], follower_ids:[], followed_user_ids: []})
@@ -10,8 +10,15 @@ export function User({current_user}) {
 
   const fetchUserData = ()=> fetch(`/user/${username}`)
     .then(r=>r.json()).then(data=>{ setUserData(data) })
-
+  // eslint-disable-next-line
   useEffect(() => { fetchUserData() }, [username])
+
+  // ! temporary "live" (every 5 second) updating
+  useEffect(() => {
+    const interval = setInterval(fetchUserData, 5000);
+    return () => clearInterval(interval);
+    // eslint-disable-next-line
+  }, []);
 
   return <div>
     <Heading text={username} showBackButton={true} showChirpCount={true} chirpCount={userData.chirp_ids.length}/>
